4740082f6a807471bfcc41f7f6502e069428b256
MariaGoncalves15 <maria.goncalves@estudante.ifro.edu.br>
2025-02-13 11:34:22 -0400

    salvaratualização

--- API_Libertadores/index.js
+++ API_Libertadores/index.js
@@ -1,10 +1,50 @@
+import cors from 'cors';
 import express from 'express';
-import pool from './servico/conexao.js';
 import { retornaCampeonatos, retornaCampeonatosPorId, retornacampeonatosPorAno, retornaCampeonatosPorTime} from './servico/retornaCampeonatos_servico.js';
 import { cadastrarCampeonato } from './servico/cadastroCampeonato_servico.js';
+import { atualizaCampeonatoParcial, atualizarCampeonato } from './servico/atualizaCampeonato_servico.js';
 
 const app = express();
-app.use(express.json()); //Suport para JSON no corpo da requisição
+app.use(cors());
+app.use(express.json()); //Suport para JSON no corpo da requisição   *Para usar no projeto de Front end: para ter o cors =>   npm install cors depois disso, importar o cors import cors from 'cors' e dps colocar o app.use(cors()); //
+
+app.patch('/campeonatos/:id', async (req, res) => { 
+    const {id} = req.params; //desconstrução
+    const {campeao, vice, ano} = req.body; //desconstrução
+
+    const camposAtualizar = {};
+    if (campeao) camposAtualizar.campeao = campeao;
+    if (vice) camposAtualizar.vice = vice;
+    if (ano) camposAtualizar.ano = ano;
+
+    if (Object.keys (camposAtualizar).length === 0) {
+        res.status(400).send('Nenhum campo válido foi preenchido para a atualização!')
+    } else {
+        const resultado = await atualizaCampeonatoParcial(id, camposAtualizar)
+        if (resultado.affectedRows > 0) {
+            res.status(202).send('Registro atualizado com sucesso')
+            
+        } else {
+            res.status(400).send('Registro não encontrado')
+        }
+    }
+})
+
+app.put('/campeonatos/:id', async (req, res) => {
+    const {id} = req.params; //desconstrução
+    const {campeao, vice, ano} = req.body; //desconstrução
+
+    if (campeao == undefined || vice == undefined || ano == undefined) {
+        res.status(400).send('Todos os campos precisam ser definidos!')
+    } else {
+        const resultado = await atualizarCampeonato(id, campeao, vice, ano);
+        if (resultado.affectedRows > 0) {
+            res.status(202).send('Registro atualizado com sucesso')
+        } else {
+            res.status(400).send('Registro não encontrado!')
+        }
+    }
+})
 
 app.post('/campeonatos', async (req, res) => {
     const campeao = req.body.campeao;
